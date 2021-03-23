c0e052f47824bcb354bd36f70c6657c87c246af6
Luca <59787168+lucabecci@users.noreply.github.com>
2021-03-23 10:57:49 -0300

    fix(Project): Paper import deleted for problems with Vercel

--- src/components/information/Main.js
+++ src/components/information/Main.js
@@ -1,4 +1,4 @@
-import { Container, Grid, Paper, Typography } from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import useStyles from "../../style/Styles";
 
